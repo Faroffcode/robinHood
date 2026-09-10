e8f9351a83f8ca058390d175f1fa0b5bf44b755c
0CrazyLove <axelvasquez582@gmail.com>
2026-09-10 18:59:22 -0500

    fix(sync): drop YouTube-deleted songs instead of re-appending them

--- app/src/main/kotlin/com/metrolist/music/utils/SyncUtils.kt
+++ app/src/main/kotlin/com/metrolist/music/utils/SyncUtils.kt
@@ -1468,6 +1468,7 @@ class SyncUtils @Inject constructor(
                     }
                     val preservedSongs = localSongIndexesAbsentFromRemote(localIds, remoteIds)
                         .map(localSongs::get)
+                        .filter { it.setVideoId == null }
 
                     database.withTransaction {
                         database.clearPlaylist(playlistId)

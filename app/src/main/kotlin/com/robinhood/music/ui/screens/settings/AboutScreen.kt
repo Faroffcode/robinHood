b1812caffd5b212d12ba5b6d9be5a3eeda5803b7
Faroffcode <iamfaroff@proton.me>
2026-09-16 16:15:30 +0530

    Fix BorderStroke import in About screen

--- app/src/main/kotlin/com/robinhood/music/ui/screens/settings/AboutScreen.kt
+++ app/src/main/kotlin/com/robinhood/music/ui/screens/settings/AboutScreen.kt
@@ -5,6 +5,7 @@
 
 package com.robinhood.music.ui.screens.settings
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -26,7 +27,6 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.BorderStroke
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
